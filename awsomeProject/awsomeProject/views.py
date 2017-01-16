3326f9d5abcb1d94dfc6d7c4a2132e31a6f99b37
reljapaunovic <paunovic.relja@gmail.com>
2017-01-16 19:18:23 +0200

    project working on heroku, tested database too, using postresql on heroku (sqlite3 locally)

---
+++ awsomeProject/awsomeProject/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
+
+
+def index(request):
+    if not Product.objects.filter(pk=1).exists():
+        data = Product(title="Panda")
+        data.save()
+    return render(request, "index.html", {"title" : Product.objects.values('title').filter(pk=1)[0]['title']})
