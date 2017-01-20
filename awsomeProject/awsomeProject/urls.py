0731b484f527c33088f4f4532914dfdac2838b0e
reljapaunovic <paunovic.relja@gmail.com>
2017-01-20 17:05:29 +0200

    added template for main page, page where you play the game

--- awsomeProject/awsomeProject/urls.py
+++ awsomeProject/awsomeProject/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^main/', views.main, name='main'),
 ]
--- awsomeProject/awsomeProject/views.py
+++ awsomeProject/awsomeProject/views.py
@@ -8,3 +8,6 @@ def index(request):
         data = Product(title="Panda")
         data.save()
     return render(request, "index.html", {"title" : Product.objects.values('title').filter(pk=1)[0]['title']})
+
+def main(request):
+    return render(request, "main.html", {})
