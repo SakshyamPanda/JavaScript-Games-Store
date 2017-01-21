88567f5aa65bf9d30fb64b61c512a01c7b40d7d1
reljapaunovic <paunovic.relja@gmail.com>
2017-01-21 16:43:37 +0200

    bug fixed

--- awsomeProject/awsomeProject/files.py
+++ awsomeProject/awsomeProject/files.py
@@ -25,7 +25,7 @@ class RegistrationForm(forms.Form):
 
 class LoginForm(forms.Form):
 
-    username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") }))
+    username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
     password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
 
     def clean_username(self):
