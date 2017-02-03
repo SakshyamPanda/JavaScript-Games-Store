b131b877e6e0a4d7eab7364d005b920dd0c2591f
reljapaunovic <paunovic.relja@gmail.com>
2017-02-03 15:14:30 +0200

    Commented the code

--- awsomeProject/awsomeProject/files.py
+++ awsomeProject/awsomeProject/files.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import Game
 from django.utils.translation import ugettext_lazy as _
 
+# Registration form specification
 class RegistrationForm(forms.Form):
     username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
     email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
@@ -11,6 +12,7 @@ class RegistrationForm(forms.Form):
     password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
     isDeveloper = forms.BooleanField(required=False, widget=forms.CheckboxInput, label=_("Developer?"))
 
+    # invoked while checking validity of the form
     def clean_username(self):
         # Raise error if user already exists (the need to be unique)
         try:
@@ -19,18 +21,22 @@ class RegistrationForm(forms.Form):
             return self.cleaned_data['username']
         raise forms.ValidationError(_("The username already exists. Please try another one."))
 
+    # Check if repeated password is the same as other one
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
                 raise forms.ValidationError(_("The two password fields did not match."))
         return self.cleaned_data
 
+# For developers, form for uploading games
 class UploadGameForm(forms.Form):
     name = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Name"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
     url = forms.URLField(label='Url to game', required=True, widget=forms.TextInput(attrs=dict(required=True, max_length=30)))
     price = forms.FloatField(required=True, min_value=0)
     description = forms.CharField()
 
+    #If game with that name already exists, user is asked to chose another.
+    # Game names are unique
     def clean_name(self):
         try:
             game = Game.objects .get(name=self.cleaned_data['name'])
