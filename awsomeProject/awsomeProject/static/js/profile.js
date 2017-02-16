829cb3174c89bf050b73844c225bb527ea9b4c03
Sakshyam Panda <sakshyam.panda@aalto.fi>
2017-02-16 20:22:40 +0200

    changes to myprofile

--- awsomeProject/awsomeProject/static/js/profile.js
+++ awsomeProject/awsomeProject/static/js/profile.js
@@ -4,4 +4,19 @@ $(".btn-pref .btn").click(function () {
     // $(".tab").addClass("active"); // instead of this do the below
     $(this).removeClass("btn-default").addClass("btn-primary");
 });
+
+/* For add game button on Manage Profile */
+$('#add-game-btn').click(function (){
+	$('#clear-btn').show();
+	$('#new-game').show();  //new-game is the create game section
+	$(this).hide();
+});
+
+/* For Clear button on Manage Profile */
+$('#clear-btn').click(function (){ 
+	$('#add-game-btn').show();
+	$(this).hide();
+	$('#new-game').hide();  //new-game is the create game section
+});
+
 });
